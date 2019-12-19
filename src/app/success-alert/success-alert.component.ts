a02de75d239586513d27caa8970b9a12f21ccd65
Marcus Mayo <marcus.mayo@medhub.com>
2019-12-19 07:09:59 -0600

    Assignment 1 complete

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-success-alert',
-    templateUrl: './success-alert.component.html',
+    template: '<p>Success!</p>',
     styleUrls: ['./success-alert.component.css']
 })
 export class SuccessAlertComponent implements OnInit {
--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
-  templateUrl: './warning-alert.component.html',
+  template: '<p>Warning!</p>',
   styleUrls: ['./warning-alert.component.css']
 })
 export class WarningAlertComponent implements OnInit {
